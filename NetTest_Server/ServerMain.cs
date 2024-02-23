ef7d298929cd7a753482835d7324e3afc49092d7
onovich <hjn1110@163.com>
2024-02-23 20:10:19 +0800

    <feature> add: 接收客户端连接并返回消息

--- NetTest_Server/ServerMain.cs
+++ NetTest_Server/ServerMain.cs
@@ -1,8 +1,36 @@
-namespace NetTest.Server {
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
 
-    public class ServerMain {
-        public static void Main(string[] args) {
+namespace TcpServer {
+
+    class ServerMain {
+
+        static void Main(string[] args) {
+
+            var server = new TcpListener(IPAddress.Loopback, 8080);
+            server.Start();
+            Console.WriteLine("Server has started on 127.0.0.1:8080.{0}Waiting for a connection...", Environment.NewLine);
+
+            TcpClient client = server.AcceptTcpClient();
+            Console.WriteLine("A client connected.");
+
+            NetworkStream stream = client.GetStream();
+
+            // Send a message to the client    
+            string message = "Hello from the server!";
+            byte[] data = Encoding.ASCII.GetBytes(message);
+
+            stream.Write(data, 0, data.Length);
+            Console.WriteLine("Sent: {0}", message);
+
+            // Shutdown and end connection
+            client.Close();
+            server.Stop();
+
         }
+
     }
 
 }
